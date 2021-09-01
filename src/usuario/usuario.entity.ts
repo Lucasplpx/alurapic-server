e63e13bf364fd61eb0288a2501e403a0b5aaba28
Lucas Passos <lucasplpx@gmail.com>
2021-09-01 20:04:02 -0300

    validacoes

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,8 +1,23 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class Usuario {
   id: number;
+
+  @IsNotEmpty({
+    message: 'nomeDeUsuario é obrigatório',
+  })
+  @IsString()
   nomeDeUsuario: string;
+
+  @IsNotEmpty()
+  @IsString()
   nomeCompleto: string;
+
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
   senha: string;
+
   dataDeEntrada: Date;
 }
