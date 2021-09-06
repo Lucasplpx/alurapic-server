70884a3a9ccdd315fff47a89db9aa8c1cf04fe3a
Lucas Passos <lucasplpx@gmail.com>
2021-09-06 09:05:22 -0300

    interceptors e serializacao

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,9 +1,13 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { Exclude, Expose } from 'class-transformer';
 import { IsNomeDeUsuarioUnico } from './is-nome-de-usuario-unico';
 
 export class Usuario {
   id: number;
 
+  @Expose({
+    name: 'username',
+  })
   @IsNomeDeUsuarioUnico({
     message: 'nomeDeUsuario precisa ser único.',
   })
@@ -13,15 +17,30 @@ export class Usuario {
   @IsString()
   nomeDeUsuario: string;
 
+  @Expose({
+    name: 'fullName',
+  })
   @IsNotEmpty()
   @IsString()
   nomeCompleto: string;
 
+  @Expose({
+    name: 'email',
+  })
   @IsEmail()
   email: string;
 
+  @Expose({
+    name: 'password',
+  })
+  @Exclude({
+    toPlainOnly: true,
+  })
   @IsNotEmpty()
   senha: string;
 
+  @Expose({
+    name: 'joinDate',
+  })
   dataDeEntrada: Date;
 }
